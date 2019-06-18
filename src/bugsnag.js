50238ab00912e1dac17c86c8d2e4656eb47e01de
Charles <charlesj@gmail.com>
2019-06-18 11:38:21 +0530

    Fallback to 'dummy' if bugsnag api key not set in .env

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -2,7 +2,7 @@ import React from 'react';
 import bugsnag from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
 
-const bugsnagClient = bugsnag(process.env.REACT_APP_BUGSNAG_API_KEY);
+const bugsnagClient = bugsnag(process.env.REACT_APP_BUGSNAG_API_KEY || 'dummy');
 bugsnagClient.use(bugsnagReact, React);
 
 const ErrorBoundary = bugsnagClient.getPlugin('react');
