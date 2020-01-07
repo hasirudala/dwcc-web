d2c86f9478d9352c68c3b37ad5efe152f40031bd
Charles <charlesj@gmail.com>
2020-01-07 12:41:55 +0530

    Modify wasteBuyer

--- src/admin/resources/wasteBuyers.js
+++ src/admin/resources/wasteBuyers.js
@@ -2,7 +2,9 @@ import React from "react"
 import {
     List, Datagrid, TextField, Create, Edit, SimpleForm, EditButton,
     TextInput, Show, SimpleShowLayout, ReferenceField, ReferenceInput,
-    SelectInput
+    SelectInput, BooleanField, BooleanInput, ShowButton, ReferenceArrayField,
+    SingleFieldList, ChipField, ReferenceArrayInput, SelectArrayInput,
+    FormDataConsumer
 } from 'react-admin'
 import { isRequired } from "../utils/validators"
 import ActionsWithBackButton from "../components/ActionsWithBackButton"
@@ -18,6 +20,11 @@ export const ListWasteBuyers = (props) =>
             <ReferenceField source="regionId" reference="regions" link="show" label="Region">
                 <TextField source="name" />
             </ReferenceField>
+            <ReferenceField source="zoneId" reference="zones" link="show" label="Zone">
+                <TextField source="name" />
+            </ReferenceField>
+            <BooleanField source="isKspcbAuthorized" label="Authorized by KSPCB" />
+            <ShowButton />
             <EditButton />
         </Datagrid>
     </List>
@@ -30,6 +37,19 @@ export const ShowWasteBuyer = (props) =>
             <ReferenceField source="regionId" reference="regions" link="show" label="Region">
                 <TextField source="name" />
             </ReferenceField>
+            <ReferenceField source="zoneId" reference="zones" link="show" label="Zone">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="address" />
+            <BooleanField source="isKspcbAuthorized" label="Authorized by KSPCB" />
+            <ReferenceArrayField label="Accepted items"
+                                 reference="wasteItems"
+                                 source="acceptedItemIds"
+            >
+                <SingleFieldList>
+                    <ChipField source="name" />
+                </SingleFieldList>
+            </ReferenceArrayField>
         </SimpleShowLayout>
     </Show>
 
@@ -55,8 +75,53 @@ const CreateEditForm = props =>
         <Div50>
             <TextInput source="name" label="Name" validate={isRequired} fullWidth />
             <LineBreak n={3} />
-            <ReferenceInput label="Region" source="regionId" reference="regions">
-                <SelectInput optionText="name" validate={isRequired} fullWidth />
-            </ReferenceInput>
+            <FormDataConsumer>
+                {formDataProps => <RegionAndZonesInput {...formDataProps} />}
+            </FormDataConsumer>
+            <TextInput source="address" label="Address" multiline fullWidth resettable options={{ rows: 3 }} />
+            <BooleanInput source="isKspcbAuthorized" label="Authorized by KSPCB" />
+            <ReferenceArrayInput source="acceptedItemIds"
+                                 reference="wasteItems"
+                                 label="Accepted Items"
+                                 sort={{ field: 'name', order: 'ASC' }}
+            >
+                <SelectArrayInput optionText="name" fullWidth />
+            </ReferenceArrayInput>
         </Div50>
     </SimpleForm>
+
+const RegionAndZonesInput = ({ formData, ...rest }) => {
+    return (
+        <>
+            <ReferenceInput label="Region"
+                            source="regionId"
+                            reference="regions"
+                            sort={{ field: 'name', order: 'ASC' }}
+                            {...rest}
+            >
+                <SelectInput optionText="name" validate={isRequired} optionValue="id" fullWidth resettable />
+            </ReferenceInput>
+            <ReferenceInput source="zoneId"
+                            reference="zones"
+                            resource="wards"
+                            label="Zones"
+                            sort={{ field: 'name', order: 'ASC' }}
+                            {...rest}
+            >
+                <ZoneSelect linkedRegionId={formData.regionId} />
+            </ReferenceInput>
+        </>
+    )
+}
+
+
+const ZoneSelect = ({ choices, linkedRegionId, ...props }) => {
+    const _choices = choices.filter(zone => zone.regionId === linkedRegionId)
+    return (
+        <SelectInput optionText="name"
+                     choices={_choices}
+                     fullWidth
+                     {...props}
+        />
+    )
+}
